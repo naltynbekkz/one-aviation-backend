5f9a07eeb65a205e33c4239eaa950bc6229614ae
Dadukazhb <54827694+Dadukazhb@users.noreply.github.com>
2021-11-08 18:47:34 +0600

    Update FlightController.kt

--- src/main/kotlin/com/naltynbekkz/oneaviation/flight/FlightController.kt
+++ src/main/kotlin/com/naltynbekkz/oneaviation/flight/FlightController.kt
@@ -9,5 +9,80 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/flights")
 class FlightController(
     private val flightRepository: FlightRepository,
-    private val sessionManager: SessionManager,
-)
+    private val sessionManager: SessionManager,)
+{
+    
+    @PostMapping
+    fun createFlight(
+        @RequestHeader(value = "Authorization", required = false) tokenId: String?,
+        @RequestBody request: CreatePlaneRequest,
+        response: HttpServletResponse,
+    ): Flight {
+        val token = sessionManager.getToken(tokenId, response, listOf(Role.MANAGER))
+        val flight = PlaneEntity(
+            id = null,
+            plane = request.plane,
+            timestamp = request.timestamp,
+            departure = request.departure,
+            arrival = request.arrival,
+            
+            
+//     author = token.user
+//     val id: Long,
+//     val plane: Plane,
+//     val timestamp: Timestamp,
+//     val departure: Info,
+//     val arrival: Info,
+        )
+        return flightRepository.save(plane).toFlight()
+    }
+
+    @GetMapping
+    fun getAllFlights(
+        @RequestParam(value = "page", required = false) page: Int?,
+        @RequestParam(value = "size", required = false) size: Int?,
+        response: HttpServletResponse,
+    ): Page<Flight> {
+        return flightRepository.getNotDeleted(pageParams(page, size)).toPage { it.toFlight() }
+    }
+
+    @PutMapping
+    fun putFlight(
+        @RequestHeader(value = "Authorization", required = false) tokenId: String?,
+        @RequestBody request: Flight,
+        response: HttpServletResponse,
+    ): Flight {
+
+        val oldPlane = planeRepository.findById(request.id).get()
+        val token = sessionManager.getToken(tokenId, response, listOf(Role.MANAGER))
+
+        oldFlight.id = request.id
+        oldFlight.plane = request.plane
+        oldFlight.timestamp = request.timestamp
+        oldFlight.departure = request.departure
+        oldFlight.arrival = request.arrival
+        oldFlight.timestamp.update()
+        
+//         id = null,
+//         plane = request.plane,
+//         timestamp = request.timestamp,
+//         departure = request.departure,
+//         arrival = request.arrival,
+        
+
+        return flightRepository.save(oldFlight).toFlight()
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteFlight(
+        @RequestHeader(value = "Authorization", required = false) tokenId: String?,
+        @PathVariable id: Long,
+        response: HttpServletResponse,
+    ) {
+        val flight = planeRepository.findById(id).get()
+        sessionManager.getToken(tokenId, response, listOf(Role.MANAGER))
+        flight.timestamp.delete()
+        flightRepository.save(flight)
+    }
+
+}
